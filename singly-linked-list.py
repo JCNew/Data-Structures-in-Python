00175d6a77bf3e8b64126bd89e57b8d9262792ee
JCNew <jcnew16@gmail.com>
2021-08-25 19:35:44 +0100

    added comments for better code explaining

--- singly-linked-list.py
+++ singly-linked-list.py
@@ -10,6 +10,12 @@ class Node:
 
 
 # SINGLY LINKED LIST IMPLEMENTATION
+'''Time complexities:
+        Searching, Accessing: O(n) (same concept since you have to iterate and can't use indexes),
+        Inserting: O(1),
+        Removing: O(1),
+        '''
+
 
 class SinglyLinkedList:
     def __init__(self, nodes):
@@ -25,27 +31,33 @@ class SinglyLinkedList:
                 current = current.next
 
     def __iter__(self):
+        # Makes sure for loops are usable in this data structure
         current = self.head
         while current is not None:
             yield current.value
             current = current.next
 
     def __str__(self):
+        # Using the print() function allows it to display a representation of the list
         return " -> ".join(str(node) for node in self)
 
     def __len__(self):
+        # Allows the len() function to be used
         return len(tuple(iter(self)))
 
     def convert_to_node(self, value):
+        # Converts the values into nodes where necessary
         return Node(value)
 
     def insert_in_head(self, node):
+        # Inserts a node in self.head, can also be implemented using the insert_in_nth() method
         if not isinstance(node, Node):
             node = self.convert_to_node(node)
         node.next = self.head
         self.head = node
 
     def insert_in_tail(self, node):
+        # Inserts a node in the last slot, can also be implemented using the insert_in_nth() method
         if not isinstance(node, Node):
             node = self.convert_to_node(node)
         if not self.head:
@@ -57,6 +69,7 @@ class SinglyLinkedList:
         current.next = node
 
     def insert_in_nth(self, index, node):
+        # Inserts a node on the nth position
         if not (0 <= index <= len(self)):
             raise IndexError("List index out of range")
         if not isinstance(node, Node):
@@ -74,6 +87,7 @@ class SinglyLinkedList:
             current.next = node
 
     def remove_nth(self, index):
+        # Removes node in nth position, can also be used to remove head or tail
         if not (0 <= index <= len(self)):
             raise IndexError("List index out of range")
         current = self.head
@@ -88,12 +102,14 @@ class SinglyLinkedList:
         return deleted_node.value
 
     def is_empty(self):
+        # Checks if the linked list is empty
         return not self.head
 
-    def search(self, value):
+    def access(self, value):
+        # Looks for a value inside the list and returns its node if it is found
         current = self.head
         while current.next is not None:
             if current.value == value:
-                return value
+                return current
             current = current.next
         return False
